05fa6fbbc2011307fd5cfa727c6b5dc452f55fd0
Yuki SHIDA <shida@be-agile.jp>
2013-01-23 20:24:52 +0900

    generatorでrspecになるように修正

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,10 @@ module P130121TwitterTdd
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework      :rspec, fixture: true, views: false
+      g.fixture_replacement :factory_girl, dir: 'spec/support/factories'
+    end
   end
 end
